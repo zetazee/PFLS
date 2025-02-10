1289172220cec2ab99d5e9d53ef1efd70936c7f7
Zahra Amini <amini.zee@protonmail.com>
2025-02-10 08:32:30 +0000

    gc resolved

--- EXC-003/fasta-file-processor.sh
+++ EXC-003/fasta-file-processor.sh
@@ -4,7 +4,7 @@ total_len=$(awk '!/>/ {printf("%s", $0)}' "$1" | wc -m)
 
 longest_seq=$(awk '/>/{if (NR==1) {print} else{printf("\n%s\n",$0)}next} {printf("%s", $0)}' "$1" | awk '!/>/{if (length > max) max = length; next}END{print max}')
 
-shortest_seq=$(awk '/^>/{if (NR==1) {print} else{printf("\n%s\n", $0)}next} {printf("%s", $0)}' "$1" | awk '!/>/{print length}' "$1" | sort | head -n 1)
+shortest_seq=$(awk '/^>/{if (NR==1) {print} else{printf("\n%s\n", $0)}next} {printf("%s", $0)}' "$1" | awk '!/>/{print length}' | sort | head -n 1)
 
 sum_len=$(awk '/^>/{if (NR==1) {print} else{printf("\n%s\n", $0)}next} {printf("%s", $0)}' "$1" | awk '!/>/{sum += length}END{print sum}' "$1")
 avg_len=$(($sum_len/$seq_num))
@@ -12,8 +12,7 @@ avg_len=$(($sum_len/$seq_num))
 G_content=$(grep -o "[Gg]" "$1" | wc -l)
 C_content=$(grep -o "[Cc]" "$1" | wc -l)
 G_C_content=$(($G_content + $C_content))
-GC_content=$(($G_C_content/$total_len))
-# bc
+GC_content=$(bc <<< "scale=2; ($G_C_content/$total_len) * 100")
 
 echo "FASTA File Statistics:"
 echo "----------------------"
@@ -22,4 +21,4 @@ echo "Total length of sequences: $total_len"
 echo "Length of the longest sequence: $longest_seq"
 echo "Length of the shortest sequence: $shortest_seq"
 echo "Average sequence length: $avg_len"
-echo "GC Content (%): $(($GC_content*100))"
+echo "GC Content (%): $GC_content"
