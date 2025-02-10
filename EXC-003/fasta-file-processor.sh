0993c4c2874e29cf2c7d30b9911ba9f49d77b0f2
Zahra Amini <amini.zee@protonmail.com>
2025-02-10 08:59:47 +0000

    fixed longest

--- EXC-003/fasta-file-processor.sh
+++ EXC-003/fasta-file-processor.sh
@@ -2,12 +2,12 @@ seq_num=$(awk '/>/ {print}' "$1" | wc -l)
 
 total_len=$(awk '!/>/ {printf("%s", $0)}' "$1" | wc -m)
 
-longest_seq=$(awk '/>/{if (NR==1) {print} else{printf("\n%s\n",$0)}next} {printf("%s", $0)}' "$1" | awk '!/>/{if (length > max) max = length; next}END{print max}')
+longest_seq=$(awk '/>/{if (NR==1) {print} else{printf("\n%s\n",$0)}next} {printf("%s", $0)}' "$1" | awk '!/>/{if (length > max) max = length; next}END{print max}' "$1")
 
 shortest_seq=$(awk '/^>/{if (NR==1) {print} else{printf("\n%s\n", $0)}next} {printf("%s", $0)}' "$1" | awk '!/>/{print length}' | sort | head -n 1)
 
 sum_len=$(awk '/^>/{if (NR==1) {print} else{printf("\n%s\n", $0)}next} {printf("%s", $0)}' "$1" | awk '!/>/{sum += length}END{print sum}' "$1")
-avg_len=$(($sum_len/$seq_num))
+avg_len=$(bc <<< "scale=2; ($sum_len/$seq_num)")
 
 G_content=$(grep -o "[Gg]" "$1" | wc -l)
 C_content=$(grep -o "[Cc]" "$1" | wc -l)
